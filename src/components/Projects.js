28f0f79949f55d989d46d1e2961a62d4372ddd21
marioamandio <marioamandio@gmail.com>
2019-07-07 23:31:26 +0100

    updated resume

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
+
 import styled from "styled-components"
 import { Grid } from "semantic-ui-react"
 
